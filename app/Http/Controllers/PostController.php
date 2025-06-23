566ada5f947b6af13de60efc576a92e6e6c1badd
githubのユーザー名 <github>
2025-06-23 13:52:25 +0000

    hyouzinotuika

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -11,38 +11,41 @@ class PostController extends Controller
 {
     public function index(Post $post)
     {
-
         return view('posts.index')->with(['posts' => $post->getPaginateByLimit()]);
-
     }
 
     public function show(Post $post)
     {
         return view('posts.show')->with(['post' => $post]);
     }
-
-
-    public function create()
+public function create()
     {
         return view('posts.create');
     }
-    public function edit(Post $post)
-    {
-    return view('posts.edit')->with(['post' => $post]);
-    }
 
-    public function update(PostRequest $request, Post $post)
-    {
-    $input_post = $request['post'];
-    $post->fill($input_post)->save();
-
-    return redirect('/posts/' . $post->id);
-    }
-
-    public function store(Post $post, PostRequest $request) // 引数をRequestからPostRequestにする
+    public function store(Post $post, PostRequest $request)
     {
         $input = $request['post'];
         $post->fill($input)->save();
         return redirect('/posts/' . $post->id);
+    } 
+
+    public function edit(Post $post)
+    {
+        return view('posts.edit')->with(['post' => $post]);
+    }
+
+    public function update(PostRequest $request, Post $post)
+    {
+$input_post = $request['post'];
+$post->fill($input_post)->save();
+
+    return redirect('/posts/' . $post->id);
+    }
+
+    public function delete(Post $post)
+    {
+$post->delete();
+return redirect('/');
     }
 }
